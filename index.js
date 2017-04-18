1046b5467e9726c565fb928989bae8ccc6bd343e
Yvaine Star <tjalwled12@gmail.com>
2017-04-18 19:18:02 +0900

    clear

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var mongoose = require('mongoose');
 
 var port = process.env.PORT || 3000;
 
@@ -9,31 +8,6 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-
-mongoose.connect('mongodb://testUser:testtest@ds163020.mlab.com:63020/chat_user');
-mongoose.Promise = global.Promise;
-require('./config/passport')(passport);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-var flash = require('connect-flash');
-app.use(flash());
-
-var session = require('express-session');
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true
-}));
-
-app.get('/signup', function(req, res){
-  res.sendFile(__dirname + '/signup.html');
-});
-
-app.get('/login', function(req, res){
-  res.sendFile(__dirname + 'login.html');
-});
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
