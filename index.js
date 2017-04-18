6912e17404eb34f17345f6a79c040057ba0e4885
Yvaine Star <tjalwled12@gmail.com>
2017-04-18 17:31:33 +0900

    start login

--- index.js
+++ index.js
@@ -1,51 +1,38 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
-var bcrypt = require('bcrypt');
+
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-app.use(bodyparser.urlencoded({ extended: false}));
+
+mongoose.connect('mongodb://testUser:testtest@ds163020.mlab.com:63020/chat_user');
+mongoose.Promise = global.Promise;
+require('./config/passport')(passport);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+var flash = require('connect-flash');
+app.use(flash());
+
+var session = require('express-session');
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
 
 app.get('/signup', function(req, res){
   res.sendFile(__dirname + '/signup.html');
 });
 
-app.post('/signup', function(req, res){
-
-  mongoose.connect('mongodb://testUser:testtest@ds163020.mlab.com:63020/chat_user');
-
-  var Schema = mongoose.Schema;
-  
-  var userSchema = new Schema({
-      username : {type:String,required:true,unique:true},
-      email : {type:String,required:true},
-      password : {type:String, required:true}
-  });
-
-  var User = mongoose.model('User', userSchema);
-
-  var salt = bcrypt.genSaltSync(12);
-  var hash = bcrypt.hashSync(req.body.password, salt);
-
-  var newUser = User({
-    username : req.body.username,
-    email : req.body.email,
-    password : hash
-  });
-
-  newUser.save(function(err){
-    if (err) throw err;
-
-    console.log('person saves!');
-  });
-
-  res.send('Registration success!');
+app.get('/login', function(req, res){
+  res.sendFile(__dirname + 'login.html');
 });
 
 io.on('connection', function(socket){
