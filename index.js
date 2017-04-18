046771628a9873953f9682ef85f8721925caf68b
Yvaine Star <tjalwled12@gmail.com>
2017-04-18 22:03:33 +0900

    Login : temporary end

--- index.js
+++ index.js
@@ -4,15 +4,24 @@ var io = require('socket.io')(http);
 var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var port = process.env.PORT || 3000;
 
 var User = require('./models/User');
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
 app.use(bodyparser.urlencoded({ extended: false}));
+app.use(session({
+ secret: '@#@$MYSIGN#@$#$',
+ resave: false,
+ saveUninitialized: true
+}));
+
+app.get('/', function(req, res){
+  var username = req.session.username;
+  console.log(username);
+  res.sendFile(__dirname+'/index.html', {username:username});
+});
 
 mongoose.connect('mongodb://testUser:testtest@ds163020.mlab.com:63020/chat_user');
 mongoose.Promise = global.Promise;
@@ -37,8 +46,7 @@ app.post('/signup', function(req, res){
 
     console.log('person saves!');
   });
-
-  res.send('Registration success!');
+  res.redirect('/login');
 });
 
 app.get('/login', function(req, res){
@@ -48,25 +56,22 @@ app.get('/login', function(req, res){
 app.post('/login', function(req, res){
   var username = req.body.username;
   var password = req.body.password;
-
+  
   User.findOne({username:username}, function(err, obj){
 
+    if (err) {
+      throw err;
+      res.redirect('/login');
+    }
+
     if (bcrypt.compareSync(password, obj.password)){
-      console.log('log in success!');
+      req.session.username = username;
+      res.redirect('/');
     }  else{
-      console.log('log in fail!');
-      console.log(obj.password, password);
+      console.log('fail!');
+      res.sendFile(__dirname+'/login.html');
     }
   });
-  /*if (User.findOne({username:username}).count()==1){
-    if(bcrypt.compareSync(hash, User.findOne({username:username}).password)){
-      res.sendFile(__dirname + '/index.html');
-    } else{
-      res.sendFile(__dirname + '/login.html');
-    }
-  } else {
-    res.sendFile(__dirname + '/login.html');
-  }*/
 });
 
 io.on('connection', function(socket){
