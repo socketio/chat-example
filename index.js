ac74bea5dd961e1ad87f67118abe99500d0a6ed7
Amir <amiretemad2000@gmail.com>
2017-04-28 22:45:10 +0430

    Change Template to bootstrap

--- index.js
+++ index.js
@@ -3,16 +3,23 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+io.on('connection', function (socket) {
+    // Display Log in Terminal
+    console.log(" A User connected !");
+    // Chat Message Event
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+    // Disconnect Event
+    socket.on('disconnect', function () {
+        console.log(" User disconnected !");
+    });
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(port, function () {
+    console.log('listening on *:' + port);
 });
