38059ff91d1aea158fab03b2de228fae36997388
Ranganath Sundar <ranganath@biobright.org>
2017-10-11 11:00:51 -0400

    circleci YML changes & added installer test

--- test/test.js
+++ test/test.js
@@ -25,7 +25,7 @@ const exec = require('child_process').exec
 
 
 describe('Build binary with pkg', function(done) {
-  this.timeout(80000)
+  this.timeout(60000)
   before(function(done) {
     exec('npm run dist', (err, stdout, stderr) => {
       assert.equal(err, null, 'Binary build failed')
@@ -45,8 +45,8 @@ describe('Build binary with pkg', function(done) {
     })
   })
 })
-/*describe('Build installer with wix', function(done) {
-  this.timeout(30000)
+describe('Build installer with wix', function(done) {
+  this.timeout(60000)
   before(function(done) {
     exec('npm run wix', (err, stdout, stderr) => {
       assert.equal(err, null, 'Installer build failed')
@@ -55,14 +55,12 @@ describe('Build binary with pkg', function(done) {
     })
   })
 
-  let targets = ['linux', 'macos', 'win.exe']
-  targets.forEach(function(target) {
-    it(`Build ${target}`, function(done) {
-      let binaryFile = `./bin/socket-chat-example-${target}`
-      fs.stat(binaryFile, function(err, stats) {
-        assert.equal(err, null, `binary file not produced, ${binaryFile}`)
-        done()
-      })
+  //let targets = ['linux', 'macos', 'win.exe']
+  it(`Build .msi`, function(done) {
+    let msiFile = `./Wix/chatAppInstaller.msi`
+    fs.stat(msiFile, function(err, stats) {
+      assert.equal(err, null, `.msi installer not generated, ${msiFile}`)
+      done()
     })
   })
-})*/
+})
