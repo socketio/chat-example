661a5c0778d9ab184e32a81f110828d36f9cdee6
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-01-23 10:42:45 -0500

    Identity based on socket.id works (#7)
    * Add message identification
    
    * Multiple changes
    
    - Move chat message formatter to a function
    - Read usernames and passwords from a plaintext JSON object in the environment named USERS (currently unused)
    - Fix the welcome/join messages
    
    * Edit with JSHint
    
    Fix 2 warnings and 3 errors

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+const users = process.env.USERS ? JSON.parse(process.env.USERS) : {"admin": "adminpassword", "user": "userpassword"};
+
+const format_msg = msg => msg.replace("\\\\", "\f") // temp rm \\
+                             .replace("\\r\\n", "\n")
+                             .replace("\\r", "\\n")
+                             .replace("\\n", "<br/>")
+                             .replace("\\t", "\t")
+                             .replace("\f", "\\\\")
+                             .split("<br/>");
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -12,16 +22,9 @@ app.get("/favicon.ico", (req, res) => {
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    msg.replace("\\\\", "\f") // temp rm \\
-       .replace("\\r\\n", "\n")
-       .replace("\\r", "\\n")
-       .replace("\\n", "<br/>")
-       .replace("\\t", "\t")
-       .replace("\f", "\\\\")
-       .split("<br/>")
-       .map((m) => {io.emit("chat message", m);})
-  });
+  socket.emit("chat message", `! Welcome, <${socket.id}>`);
+  socket.broadcast.emit("chat message", `! <${socket.id}> has joined.`);
+  socket.on('chat message', msg => format_msg(msg).map((m) => {io.emit("chat message", `% <${socket.id}> ${m}`);}));
 });
 
 http.listen(port, function(){
