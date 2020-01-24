e104bd474b56ca21a9ff9706ee9371eaf9f21ce2
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-01-24 15:28:38 -0500

    Merge the usernames (#8)
    * Add message identification
    
    * Multiple changes
    
    - Move chat message formatter to a function
    - Read usernames and passwords from a plaintext JSON object in the environment named USERS (currently unused)
    - Fix the welcome/join messages
    
    * Edit with JSHint
    
    Fix 2 warnings and 3 errors
    
    * Set default name to a slice of ID
    
    And add potential for renaming
    
    * Add leave message
    
    ! <Commit Message> has left.
    
    * Experiment with telling everyone that someone left
    
    Instead of broadcasting it
    
    * Implement possible name change support
    
    * Flip the ternary branches
    
    * Add the sender thingy
    
    * Add names
    
    theop
    Freshdude
    RedRaptor69
    
    * Fix the names
    
    * 2 changes
    
    Fix param name in apply_name()
    Format apply_name()
    
    * Fix the constant name
    
    * Fix name
    
    DarkWolFR9 -> DarkWolF129
    
    * you really have to be stubborn, dark
    
    * save name
    
    * Update index.js
    
    * Update index.js
    
    * Revert the inappropriate actions of Adam
    
    * typo, oof...
    
    * save
    
    * Do things
    
    I did a couple of things
    
    * Get a JS hint
    
    jshint.com
    
    * Turn off the who dis bot
    
    * Turn off who dis bot
    
    * Comment out who dis bot
    
    Co-authored-by: CodingPerson123 <59974531+CodingPerson123@users.noreply.github.com>

--- index.js
+++ index.js
@@ -5,6 +5,48 @@ var port = process.env.PORT || 3000;
 
 const users = process.env.USERS ? JSON.parse(process.env.USERS) : {"admin": "adminpassword", "user": "userpassword"};
 
+const names = {};
+
+/*
+const whoDisBot = {
+  botName: "WhoDisBot",
+  onJoin: (socket) => {
+    setTimeout(this.whoDis, 1000*Math.random());
+  },
+  whoDis: () => {
+    io.emit("chat message", `& <${this.botName}> who dis?`);
+  },
+  
+  onLeave: (socket) => {
+    setTimeout(this.whoDat(socket), 1000*Math.random());
+  },
+  whoDat: () => {
+    io.emit("chat message", `& <${this.botName}> who dat?`);
+  }
+};
+*/
+
+const apply_name = (who, name) => {
+  who.broadcast.emit("chat message", `! ${names[who.id]} has applied name ${name}.`);
+  names[who.id] = name;
+  who.emit("chat message", "@ Name applied successfully.");
+};
+
+const magic = (sender, msg) => {
+  switch (msg) {
+    case "/iam theop":
+      apply_name(sender, "RootUser213"); return true;
+    case "/iam Freshdude":
+      apply_name(sender, "DarkWolf129"); return true;
+    case "/iam Adam":
+      sender.disconnect(); return true;
+      //apply_name(sender, "PoopyFace"); return true;
+    case "/iam pokepat12":
+      apply_name(sender, "Poképat12"); return true;
+    default:
+      return false;
+  }
+};
 const format_msg = msg => msg.replace("\\\\", "\f") // temp rm \\
                              .replace("\\r\\n", "\n")
                              .replace("\\r", "\\n")
@@ -22,6 +64,21 @@ app.get("/favicon.ico", (req, res) => {
 });
 
 io.on('connection', function(socket){
+  names[socket.id] = socket.id.slice(0,8);
+  socket.emit("chat message", `! Welcome, <${names[socket.id]}>`);
+  socket.broadcast.emit("chat message", `! <${names[socket.id]}> has joined.`);
+  //whoDisBot.onJoin(socket);
+  socket.on('chat message', msg => (
+                                   magic(socket, msg) ?
+                                   undefined :
+                                   format_msg(msg).map((m) => {io.emit("chat message", `% <${names[socket.id]}> ${m}`);})
+                                   ));
+  socket.on("disconnect", () => {
+    io.emit("chat message", `! <${names[socket.id]}> has left.`);
+    //whoDisBot.onLeave(socket);
+    names[socket.id] = undefined;
+  });
+=======
   socket.emit("chat message", `! Welcome, <${socket.id}>`);
   socket.broadcast.emit("chat message", `! <${socket.id}> has joined.`);
   socket.on('chat message', msg => format_msg(msg).map((m) => {io.emit("chat message", `% <${socket.id}> ${m}`);}));
